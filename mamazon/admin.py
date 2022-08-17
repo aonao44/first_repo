3004f3cd2382654edc07cf4bce7d383db3d989eb
naoki <aokinao44@gmail.com>
2022-08-17 22:49:02 +0900

    lesson04_01

--- mamazon/admin.py
+++ mamazon/admin.py
@@ -1,3 +1,7 @@
+from atexit import register
+from itertools import product
 from django.contrib import admin
+from . models import Product
 
 # Register your models here.
+admin.site.register(Product)
